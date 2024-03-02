f07231dc48b4af8f6be271a17c7bf17d31e81115
Benjamin Charity <ben.charity@hey.com>
2024-03-01 22:14:39 -0500

    feat: update footer with copywrite

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,16 +1,25 @@
 export function Footer() {
   return (
-    <footer className={'group pb-2 pt-4'}>
+    <footer className={'space-y-2 pb-2 pt-8 font-mono text-xs'}>
       <p className={'text-center'}>
         <abbr
           className={
-            'cursor-help text-xs no-underline opacity-80 transition-all group-hover:text-base group-hover:opacity-100 dark:opacity-80'
+            'cursor-help no-underline opacity-80 transition-all dark:opacity-80'
           }
           title="Building better. Startups & strategy."
         >
           🛠️ 🌟 🎯
         </abbr>
       </p>
+
+      <div
+        className={
+          'mx-auto flex flex-col justify-center gap-0 text-center align-middle md:flex-row md:justify-center md:gap-4'
+        }
+      >
+        <div>Copyright © {new Date().getFullYear()} Benjamin Charity.</div>
+        <div>All rights reserved.</div>
+      </div>
     </footer>
   );
 }
